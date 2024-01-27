0cc67d584e782ab8ca2b7d4b23f24de11f461768
nallam sunilkumar <nn4399@srmist.edu.in>
2024-01-27 17:20:35 +0530

    [sunil] completed problem 5

--- Program.cs
+++ Program.cs
@@ -10,23 +10,21 @@ namespace Array_Problem6
     {
         static void Main(string[] args)
         {
-            int n = 7;
-            for (int i = 0; i < n; i++)
+            int n = 8;
+            for (int i = 1; i <=n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = i; j <=n; j++)
                 {
-                    if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
-                    {
-                        Console.Write("*");
-                    }
-                    else
-                    {
-                        Console.Write(" ");
-                    }
+                    Console.Write("*");
+
                 }
+
                 Console.WriteLine();
             }
-            Console.ReadLine();
+        Console.ReadLine();
+            
         }
+
+        
     }
 }
