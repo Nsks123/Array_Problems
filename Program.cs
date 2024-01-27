d7e81d10da4615fc2ff5e769901dc509c56d9aa7
nallam sunilkumar <nn4399@srmist.edu.in>
2024-01-27 21:50:56 +0530

    [sunil] completed Problem_7

--- Program.cs
+++ Program.cs
@@ -4,27 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Array_Problem6
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            int n = 8;
-            for (int i = 1; i <=n; i++)
-            {
-                for (int j = i; j <=n; j++)
-                {
-                    Console.Write("*");
-
-                }
-
-                Console.WriteLine();
-            }
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+        string reversedString = ReverseEachWord(input);
+        Console.WriteLine("Reversed String: " + reversedString);
         Console.ReadLine();
-            
+    }
+    static string ReverseEachWord(string input)
+    {
+        string[] words = input.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = ReverseString(words[i]);
         }
+        string reversedString = string.Join(" ", words);
+        return reversedString;
+        Console.ReadLine();
+    }
+    static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+        Console.ReadLine();
 
-        
     }
 }
