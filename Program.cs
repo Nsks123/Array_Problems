b89ffc42db1475972dbbc9efe1ab109e652d03eb
nallam sunilkumar <nn4399@srmist.edu.in>
2024-01-28 18:48:22 +0530

    [sunil] Completed Problem_8

--- Program.cs
+++ Program.cs
@@ -1,36 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-class Program
+public class SumExample
 {
-    static void Main()
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a string:");
-        string input = Console.ReadLine();
-        string reversedString = ReverseEachWord(input);
-        Console.WriteLine("Reversed String: " + reversedString);
-        Console.ReadLine();
-    }
-    static string ReverseEachWord(string input)
-    {
-        string[] words = input.Split(' ');
-        for (int i = 0; i < words.Length; i++)
+        int n, sum = 0, m;
+        Console.Write("Enter a number: ");
+        n = int.Parse(Console.ReadLine());
+        while (n > 0)
         {
-            words[i] = ReverseString(words[i]);
+            m = n % 10;
+            sum = sum + m;
+            n = n / 10;
         }
-        string reversedString = string.Join(" ", words);
-        return reversedString;
+        Console.Write("Sum is= " + sum);
         Console.ReadLine();
-    }
-    static string ReverseString(string str)
-    {
-        char[] charArray = str.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
-        Console.ReadLine();
-
     }
 }
